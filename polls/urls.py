2d6ebfa221683c38e98cbcf733f979b7c8e5bb2a
Vitor Vieira <vieiravh2005@gmail.com>
2023-12-08 08:41:18 -0300

    django polls vamo que vamo

--- polls/urls.py
+++ polls/urls.py
@@ -1,24 +1,51 @@
 from django.urls import path
+
+from polls.models import Question
+
 from . import views
+
 urlpatterns = [
-    # # ex: 127.0.0.1:8000/polls/
-    # path("", views.index, name="index"),
-
-    # # ex: 127.0.0.1:8000/polls/5/
-    # path("<int:question_id>/", views.detail, name="detail"),
-
-    # # ex: 127.0.0.1:8000/polls/5/results/
-    # path("<int:question_id>/results/", views.results, name="results"),
-
-    # # ex: 27.0.0.1:8000/polls/5/vote/
-    # path("<int:question_id>/vote/", views.vote, name="vote"),
-
-    # página de cadastro de nova enquete
-    path('cadastrar', views.QuestionCreateView.as_view(), name="question-create"),   
-    path('listar', views.QuestionListView.as_view(), name="question-list"),
-    path('<int:pk>', views.QuestionDetailView.as_view(), name="question-detail"),
-    path('<int:pk>/deletar', views.QuestionDeleteView.as_view(), name="question-delete"),
-    path('<int:pk>/atualizar', views.QuestionUpdateView.as_view(), name='question-update')
-    
+    path('', views.index, name='index'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao'),
+    path('pergunta', views.ultimas_perguntas, name='ultimas_perguntas'),
+    path('pergunta/list', views.ultimas_perguntas, name='polls_list'),
+    path('pergunta/add', views.QuestionCreateView.as_view(), name="polls_add"),
+    path('pergunta/<int:pk>/edit', 
+         views.QuestionUpdateView.as_view(), 
+         name="polls_edit"),
+    path('pergunta/<int:pk>/delete', 
+         views.QuestionDeleteView.as_view(), 
+         name="polls_delete"),
+     path('pergunta/<int:pk>/show', 
+         views.QuestionDetailView.as_view(), 
+         name="polls_show"),
+     path('pergunta/all',
+          views.QuestionListView.as_view(),
+          name="polls_all"
+          ),
+     path('about-us',
+          views.SobreTemplateView.as_view(),
+          name="about_page"
+          ),
+     path('pergunta/<int:pk>/alternativa/add', 
+          views.ChoiceCreateView.as_view(), 
+          name="choice_add"
+          ),
+     path('alternativa/<int:pk>/edit', 
+          views.ChoiceUpdateView.as_view(), 
+          name="choice_edit"
+          ),
+     path('alternativa/<int:pk>/delete', 
+          views.ChoiceDeleteView.as_view(), 
+          name="choice_delete"
+          ),
+     path('pergunta/<int:question_id>/vote',
+          views.vote,
+          name="poll_vote"),
+     path('pergunta/<int:question_id>/results',
+     views.results,
+     name="poll_results"
+     )
+     
 ]
-
